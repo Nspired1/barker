55cfaa430747075f15d677d1d5acf24a8e3ad51e
Nspired1 <don.spire1@gmail.com>
2020-10-28 23:22:19 -0700

    Add Bootstrap and navbar

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import { Provider } from "react-redux";
 import { configureStore } from "../store";
-import {BrowswerRouter as Router} from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import Navbar from "./Navbar";
 
 const store = configureStore();
 
 const App = () => (
   <Provider store={store}>
     <Router>
-      <div>
+      <div className="onboarding">
+      <Navbar />
         Hello World
       </div>
     </Router>
